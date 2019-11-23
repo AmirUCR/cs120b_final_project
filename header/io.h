dbbbc96133fa663b9528124129446770b2516900
Amirsadra Mohseni <amohs002@ucr.edu>
2019-11-22 21:32:56 -0800

    Add fn declaration

--- header/io.h
+++ header/io.h
@@ -7,6 +7,7 @@ void LCD_WriteCommand (unsigned char Command);
 void LCD_WriteData(unsigned char Data);
 void LCD_Cursor (unsigned char column);
 void LCD_DisplayString(unsigned char column ,const unsigned char *string);
+void LCD_DisplayString_xy (unsigned char row, unsigned char pos, unsigned char *string);
 void delay_ms(int miliSec);
-#endif
 
+#endif
